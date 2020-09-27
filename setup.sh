9254079b0934f86b5bf38693ae30111019d56317
Will Hess <whess@anduril.com>
2020-09-27 15:53:31 -0700

    setup oh-my-zsh plugins using setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Helper functions
+dir_exists() {
+    [ -d "$@" ]
+}
 # Setup environmental variables
 export DOTDIR=$HOME/.dotfiles
 
@@ -8,7 +12,13 @@ export DOTDIR=$HOME/.dotfiles
 [ ! -L $HOME"/.vimrc" ] && ln -sv $DOTDIR"/.vim/vimrc" $HOME"/.vimrc"
 
 # Setup zsh
-[ ! -d $HOME"/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+omz=$HOME/.oh-my-zsh
+zsh_custom=$omz/custom
+dir_exists $omz || git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+dir_exists $zsh_custom/themes/powerlevel10k ||  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $zsh_custom/themes/powerlevel10k
+dir_exists $zsh_custom/plugins/zsh-autosuggestions || git clone https://github.com/zsh-users/zsh-autosuggestions $zsh_custom/plugins/zsh-autosuggestions
+dir_exists $zsh_custom/plugins/zsh-syntax-highlighting ||  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_custom/plugins/zsh-syntax-highlighting
 [ ! -L $HOME"/.zshrc" ] && ln -sv $DOTDIR"/.zsh/.zshrc" $HOME"/.zshrc"
 
-. $HOME/.zshrc
+# Start a new zsh session
+exec zsh
