a6336205d4f9c0b6d55120f90e56c2ba792d1d13
Will Hess <willphess@gmail.com>
2020-09-27 14:18:56 -0700

    add zsh configurations (#3)

--- setup.sh
+++ setup.sh
@@ -7,3 +7,8 @@ export DOTDIR=$HOME/.dotfiles
 [ ! -d $DOTDIR"/.vim/undodir" ] && mkdir $DOTDIR"/.vim/undodir"
 [ ! -L $HOME"/.vimrc" ] && ln -sv $DOTDIR"/.vim/vimrc" $HOME"/.vimrc"
 
+# Setup zsh
+[ ! -d $HOME"/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[ ! -L $HOME"/.zshrc" ] && ln -sv $DOTDIR"/.zsh/.zshrc" $HOME"/.zshrc"
+
+. $HOME/.zshrc
